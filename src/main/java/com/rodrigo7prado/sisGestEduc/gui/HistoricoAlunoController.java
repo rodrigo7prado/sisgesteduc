0cfb9a76228bf63e3924a192ca278298621032ab
rodrigo7prado <rodrigo7prado@gmail.com>
2020-03-10 13:00:55 -0300

    Implementação do fxml e esboço do controller AlunoHistorico

---
+++ src/main/java/com/rodrigo7prado/sisGestEduc/gui/HistoricoAlunoController.java
@@ -0,0 +1,19 @@
+package com.rodrigo7prado.sisGestEduc.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.Initializable;
+
+@Controller
+public class HistoricoAlunoController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
